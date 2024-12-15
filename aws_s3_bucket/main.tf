b1b57e0d4dbd2a060b6dce6e6aeb9f7eeae05a14
soliveira024 <soliveira024@hotmail.com>
2024-12-14 22:20:28 -0300

    modulo s3

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 # Gera um número aleatório de 3 dígitos
 resource "random_integer" "bucket_suffix" {
   min = 100
