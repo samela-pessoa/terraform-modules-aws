78b83aeb2154c0ab62e7437f6b40c39062050620
soliveira024 <soliveira024@hotmail.com>
2024-12-15 17:20:22 -0300

    alterando vpc

--- aws_vpc/outputs.tf
+++ aws_vpc/outputs.tf
@@ -1,9 +1,19 @@
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.this.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.vpc.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.vpc.cidr_block
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = aws_vpc.vpc.arn
 }
 
 output "route_table_id" {
-  description = "ID of the Main Route Table"
-  value       = aws_route_table.main.id
+  description = "The ID of the Route Table associated with the VPC"
+  value       = aws_route_table.route_table.id
 }
